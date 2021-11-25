b29c43dc7828c24d91bd47d47f0ff941d2609a49
camilovegag <camilovegag@unisabana.edu.co>
2021-11-25 10:01:51 -0500

    Implement redux

---
+++ redux/reducers.js
@@ -0,0 +1,28 @@
+import { UPDATE_TEXT, FILTER_BY_NAME } from "./actions";
+const data = require("../data.json");
+
+const initialState = {
+  text: []
+};
+
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case FILTER_BY_NAME:
+      if (action.payload.length == 0) {
+        return { ...state, text: [] };
+      } else {
+        return {
+          ...state,
+          text: data.users.filter((user) =>
+            user.profile.first_name
+              .toLowerCase()
+              .startsWith(action.payload.toLowerCase())
+          )
+        };
+      }
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
