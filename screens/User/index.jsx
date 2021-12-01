b745e3a22efebf463cfddcdaf2a03a2fb6246794
camilovegag <camilovegag@unisabana.edu.co>
2021-12-01 10:00:22 -0500

    Implement new skills component on User

--- screens/User/index.jsx
+++ screens/User/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Paper from "../../components/Paper";
 import { Flex, ScrollView, Text } from "native-base";
 import UserCard from "../../components/UserCard";
-import UserSkills from "../../components/UserSkills";
 import UserProject from "../../components/UserProject";
+import Skills from "../../components/Skills";
 
 const profile = {
   first_name: "Camilo",
@@ -12,38 +12,46 @@ const profile = {
   country: "Colombia",
   job_title: "Web UI Developer",
   picture: "https://i.pravatar.cc/300",
-  email: "camilo.vega@gmail.com"
+  email: "camilo.vega@gmail.com",
 };
 
 const skills = [
   {
     name: "JS",
-    value: 85
+    value: 85,
   },
   {
     name: "React",
-    value: 71
+    value: 71,
   },
   {
     name: "SQL",
-    value: 22
+    value: 22,
   },
   {
     name: "Python",
-    value: 32
+    value: 32,
   },
   {
     name: "CSS",
-    value: 92
+    value: 92,
   },
   {
     name: "HTML",
-    value: 9
+    value: 90,
   },
   {
     name: "Java",
-    value: 2
-  }
+    value: 2,
+  },
+  {
+    name: "C++",
+    value: 15,
+  },
+  {
+    name: "Firebase",
+    value: 45,
+  },
 ];
 
 const projects = [
@@ -52,16 +60,15 @@ const projects = [
     description: "Deserunt ad in qui nostrud.",
     manager: "Farrell Tillman",
     techLead: "Terry Alexander",
-    endDate: 2171092496458
+    endDate: 2171092496458,
   },
   {
     name: "et et",
-    description:
-      "Et esse minim officia et culpa qui enim eu ea ex proident sunt elit.",
+    description: "Et esse minim officia et culpa qui enim eu ea ex proident sunt elit.",
     manager: "Cathryn Dickerson",
     techLead: "Atkinson Whitley",
-    endDate: 2313211121267
-  }
+    endDate: 2313211121267,
+  },
 ];
 
 const colors = ["#C4D600", "#00AEEF", "#333333"];
@@ -72,23 +79,12 @@ const User = () => {
       <ScrollView>
         <UserCard profile={profile} />
         <Flex bg="#fff" flex={1}>
-          <UserSkills skills={skills} first_name={profile.first_name} />
-
-          <Text
-            color="#333"
-            fontFamily="Nunito-semi-bold"
-            fontSize="18px"
-            mt={8}
-            mx={4}
-          >
+          <Skills skills={skills} first_name={profile.first_name} />
+          <Text color="#333" fontFamily="Nunito-semi-bold" fontSize="18px" mt={6} mx={4}>
             Projects
           </Text>
           {projects.map((project, idx) => (
-            <UserProject
-              key={idx}
-              project={project}
-              color={colors[idx % colors.length]}
-            />
+            <UserProject key={idx} project={project} color={colors[idx % colors.length]} />
           ))}
         </Flex>
       </ScrollView>
--- screens/UserResult/index.jsx
+++ screens/UserResult/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Paper from "../../components/Paper";
 import UserCard from "../../components/UserCard";
 import UserProject from "../../components/UserProject";
-import UserSkills from "../../components/UserSkills";
+import Skills from "../../components/Skills";
 import { Flex, ScrollView, Text } from "native-base";
 
 const UserResult = ({ route }) => {
@@ -14,9 +14,9 @@ const UserResult = ({ route }) => {
     <Paper>
       <ScrollView>
         <UserCard profile={profile} />
-        <Flex bg="#fff" flex={1} borderTopRadius="16">
-          <UserSkills skills={skills} first_name={first_name} />
-          <Text color="#333" fontFamily="Nunito-semi-bold" fontSize="18px" mt={8} mx={4}>
+        <Flex bg="#fff" flex={1}>
+          <Skills skills={skills} first_name={first_name} />
+          <Text color="#333" fontFamily="Nunito-semi-bold" fontSize="18px" mt={6} mx={4}>
             Projects
           </Text>
           {projects.map((project, idx) => (
