e313de96b320b47d32aa5fb735ae7f426772371a
camilovegag <camilovegag@unisabana.edu.co>
2021-12-15 17:15:26 -0500

    Add filters to search user on name or email

--- redux/reducers.js
+++ redux/reducers.js
@@ -1,31 +1,32 @@
 import { UPDATE_TEXT, FILTER_BY_NAME } from "./actions";
-//const data = require("../data.json");
 let data;
 
-async function JSONResponse () {
-
+async function JSONResponse() {
   let promise = new Promise((resolve, reject) => {
-    var XMLHttpRequest = require('xhr2');
+    var XMLHttpRequest = require("xhr2");
     let req = new XMLHttpRequest();
     let currentData = "";
     req.open("GET", "https://api.jsonbin.io/v3/b/619fe92201558c731cc8d669/latest", true);
-    req.setRequestHeader("X-Master-Key", "$2b$10$uhR.5vVjjd1Oxt5/VemqBemLTV2utVcdwvRBW.9NuByKPccbG67Oa");
+    req.setRequestHeader(
+      "X-Master-Key",
+      "$2b$10$uhR.5vVjjd1Oxt5/VemqBemLTV2utVcdwvRBW.9NuByKPccbG67Oa"
+    );
     req.send();
     req.onreadystatechange = () => {
       if (req.readyState == XMLHttpRequest.DONE) {
-        currentData = JSON.parse(req.responseText)
-        resolve(currentData.record)
+        currentData = JSON.parse(req.responseText);
+        resolve(currentData.record);
         reject("");
       }
-    }
-  })
+    };
+  });
   data = await promise;
 }
 
 JSONResponse();
 
 const initialState = {
-  text: []
+  text: [],
 };
 
 function userReducer(state = initialState, action) {
@@ -34,14 +35,16 @@ function userReducer(state = initialState, action) {
       if (action.payload.length == 0) {
         return { ...state, text: [] };
       } else {
-        return  {
+        return {
           ...state,
-          text: data.users.filter(user=> (
-            (user.profile.first_name.toLowerCase().startsWith(action.payload.toLowerCase()))
-            ||
-            (user.profile.last_name.toLowerCase().startsWith(action.payload.toLowerCase()))
-            
-          )).sort(),
+          text: data.users
+            .filter(
+              (user) =>
+                user.profile.email.toLowerCase().startsWith(action.payload.toLowerCase()) ||
+                user.profile.first_name.toLowerCase().startsWith(action.payload.toLowerCase()) ||
+                user.profile.last_name.toLowerCase().startsWith(action.payload.toLowerCase())
+            )
+            .sort(),
         };
       }
     default:
