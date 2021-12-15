95bab772c7038e7f338f7e8278934cdf82c3c1ca
camilovegag <camilovegag@unisabana.edu.co>
2021-12-15 17:13:25 -0500

    Create conditional rendering for fields & image

--- components/SearchList/index.jsx
+++ components/SearchList/index.jsx
@@ -1,59 +1,54 @@
 import React from "react";
-import {
-  Avatar,
-  Box,
-  Button,
-  Center,
-  FlatList,
-  Flex,
-  Icon,
-  Input,
-  Text
-} from "native-base";
+import { Avatar, Box, Button, FlatList, Flex, Text } from "native-base";
 
-export default function SearchList({list,navigate})  { 
-return(
+export default function SearchList({ list, navigate }) {
+  return (
     <FlatList
-    nestedScrollEnabled
-    value=""
-    keyExtractor={(array) => array.profile.email}
-    data={list}
-    renderItem={({ item }) => {
-    return (
-        <Button
-        backgroundColor= "#333"
-        width= {300}
-        alignItems="flex-start"
-        justifyContent="flex-start"
-        onPress={() =>
-            navigate("UserResult",{
-            user : item,
-            })
-        }
-        >
-        <Flex flexDirection="row"  justifyContent="flex-start" ml={0} >
-        <Avatar
-            bg="amber.500"
-            source={{ uri: item.profile.picture }}
-            size={50}
-            marginRight={6}
-            marginLeft={0}
-        >
-            {item.profile.first_name.charAt(0)}
-            {item.profile.last_name.charAt(0)}
-        </Avatar>
-        <Box>
-            <Text fontSize="16px" fontWeight="semibold" color="white">
-            {item.profile.first_name} {item.profile.last_name} 
-            </Text>
-            <Text fontSize="12px" color="white">
-            {item.profile.email}
-            </Text>
-        </Box>
-        </Flex>
-    </Button>
-    );
-    }}
-    /> 
-)
+      nestedScrollEnabled
+      value=""
+      keyExtractor={(array) => array.profile.email}
+      data={list}
+      renderItem={({ item }) => {
+        return (
+          <Button
+            backgroundColor="#333"
+            width={300}
+            alignItems="flex-start"
+            justifyContent="flex-start"
+            onPress={() =>
+              navigate("UserResult", {
+                user: item,
+              })
+            }
+          >
+            <Flex flexDirection="row" justifyContent="flex-start" ml={0}>
+              <Avatar
+                bg="amber.500"
+                source={
+                  item.profile.picture.length !== 0
+                    ? {
+                        uri: item.profile.picture,
+                      }
+                    : { uri: "https://iupac.org/wp-content/uploads/2018/05/default-avatar.png" }
+                }
+                size={50}
+                marginRight={6}
+                marginLeft={0}
+              />
+              <Box>
+                <Text fontSize="16px" fontWeight="semibold" color="white">
+                  {item.profile.first_name.length === 0
+                    ? `N/A`
+                    : `${item.profile.first_name} ${item.profile.last_name}`}
+                </Text>
+                <Text fontSize="12px" color="white">
+                  {item.profile.email}
+                </Text>
+              </Box>
+            </Flex>
+          </Button>
+        );
+      }}
+    />
+  );
 }
--- components/UserCard/index.jsx
+++ components/UserCard/index.jsx
@@ -3,49 +3,49 @@ import { Avatar, Box, Flex, Icon, Text } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const UserCard = ({ profile }) => {
-  const { first_name, last_name, city, country, job_title, picture, email } =
-    profile;
+  let { first_name, last_name, city, country, job_title, picture, email } = profile;
 
   return (
     <Box bg="#505050" h="240px" px="16px" py="32px">
       <Flex align="center" direction="row">
-        <Avatar h="88px" mr="24px" source={{ uri: picture }} w="88px" />
+        <Avatar
+          bg="amber.500"
+          h="88px"
+          mr="24px"
+          source={
+            picture.length !== 0
+              ? {
+                  uri: picture,
+                }
+              : { uri: "https://iupac.org/wp-content/uploads/2018/05/default-avatar.png" }
+          }
+          w="88px"
+          display="flex"
+          flexDirection="row"
+        >
+          {first_name.charAt(0)}
+          {last_name.charAt(0)}
+        </Avatar>
         <Flex flex={1} h="80px">
-          <Text
-            color="#fff"
-            fontSize="22px"
-            fontWeight="semibold"
-            lineHeight="26px"
-            mb={4}
-          >
-            {first_name} {last_name}
+          <Text color="#fff" fontSize="22px" fontWeight="semibold" lineHeight="26px" mb={4}>
+            {first_name.length === 0 ? `N/A` : `${first_name} ${last_name}`}
           </Text>
           <Text color="#fff" fontSize="16px" lineHeight="20px">
-            {job_title}
+            {job_title.length === 0 ? `N/A` : job_title}
           </Text>
         </Flex>
       </Flex>
       <Flex h="64px" justify="space-between" mt="24px">
         <Flex align="center" direction="row">
-          <Icon
-            as={<MaterialIcons name="email" />}
-            color="#fff"
-            mr="12px"
-            size="24px"
-          />
+          <Icon as={<MaterialIcons name="email" />} color="#fff" mr="12px" size="24px" />
           <Text color="#fff" fontSize="14px" lineHeight="18px">
-            {email}
+            {email.length === 0 ? `N/A` : email}
           </Text>
         </Flex>
         <Flex align="center" direction="row">
-          <Icon
-            as={<MaterialIcons name="place" />}
-            color="#fff"
-            mr="12px"
-            size="24px"
-          />
+          <Icon as={<MaterialIcons name="place" />} color="#fff" mr="12px" size="24px" />
           <Text color="#fff" fontSize="14px" lineHeight="18px">
-            {city}, {country}.
+            {city.length === 0 ? `N/A` : `${city} ${country}`}
           </Text>
         </Flex>
       </Flex>
